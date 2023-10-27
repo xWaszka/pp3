eee7a101d228f00f7591da2a8af255987881adff
s-013-18 <s-013-18@gumiforest.ae.krakow.pl>
2023-10-27 18:09:39 +0200

    27.10.23 MW

--- 02-ClassesAndObjects/Speed.java
+++ 02-ClassesAndObjects/Speed.java
@@ -1,10 +1,8 @@
 public class Speed {
-    int speed;
     public static void main(String[] args) {
 
-    Speed s = new Speed();
-    s.speed = 166;
-    System.out.println(s.speed >= 40 && s.speed <= 140 ? "Speed is Valid" : "Speed is not valid");
+    int speed = 166;
+    System.out.println(speed >= 40 && speed <= 140 ? "Speed is Valid" : "Speed is not valid");
 
 
 
