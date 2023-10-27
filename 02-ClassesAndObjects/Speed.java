03cd8540201916209bd0e0b63302758af08be893
s-013-18 <s-013-18@gumiforest.ae.krakow.pl>
2023-10-27 18:06:23 +0200

    27.10.23 MW

---
+++ 02-ClassesAndObjects/Speed.java
@@ -0,0 +1,12 @@
+public class Speed {
+    int speed;
+    public static void main(String[] args) {
+
+    Speed s = new Speed();
+    s.speed = 166;
+    System.out.println(s.speed >= 40 && s.speed <= 140 ? "Speed is Valid" : "Speed is not valid");
+
+
+
+    }
+}
